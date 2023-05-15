da5e29186ff73546946d6e21008c95c21e45dfe5
Vinicius Silva Pereira <viniciussilvapereira24@gmail.com>
2023-05-15 20:59:32 -0300

    Fazendo validação de campos verificados

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -12,6 +12,8 @@ import { useHistory } from "react-router-dom";
 
 export function FormSingIn() {
    const [formData, setFormData] = useState<ILogin>({} as ILogin);
+   const [formError, setFormError] = useState<boolean>(false);
+
    const history = useHistory();
 
    function handleLogin(data: ILocalStorage) {
@@ -24,6 +26,25 @@ export function FormSingIn() {
       history.push(`/home`);
    }
 
+   function handleChangeUser(event: React.ChangeEvent<HTMLInputElement>) {
+      const { id, value } = event.target;
+      if (value.trim() === '') {
+         setFormError(true);
+         setFormData({ ...formData, [id]: value })
+      } else {
+         setFormError(false);
+         setFormData({ ...formData, [id]: value })
+      }
+   }
+
+   function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+      if (event.key === "Enter" || event.key === "NumpadEnter" || event.keyCode === 13) {
+         console.log("enter");
+         event.preventDefault();
+         handleSubmit();
+      }
+   }
+
    const handleSubmit = async () => {
 
       const data = {
@@ -31,37 +52,42 @@ export function FormSingIn() {
          password: formData.password
       };
 
-      await api.post("/login", data).then(response => {
-         if (response.status == 200){
-            handleLogin(
-               response.data
-            )
+      if (!data.username || !data.password) {
+         setFormError(true);
+         return;
+      } else {
+         await api.post("/login", data).then(response => {
+            if (response.status == 200) {
+               handleLogin(
+                  response.data
+               )
             }
-      }).catch(resposta => {
-         console.log('erro - ', resposta)
-
-         if (resposta.code == "ERR_BAD_REQUEST"){
-            alert("Usuario ou senha invalidos")              
-         }
-      })
+         }).catch(resposta => {
+            console.log('erro - ', resposta)
+   
+            if (resposta.code == "ERR_BAD_REQUEST") {
+               alert("Usuario ou senha invalidos")
+            }
+         })
+      }
    }
 
    return (
       <>
          <Container>
-            <FormContainer>
+            <FormContainer onKeyDown={handleKeyDown}>
                <InputLabelContainer>
-                  <TextLabel 
+                  <TextLabel
                      style={{
-                     color: "#FFFFFF",
-                     fontSize: pxToRem(20),
-                     fontWeight: 700,
-                  }}>Username:</TextLabel>
-
-                  <Input 
-                     placeholder="João" 
-                     className="inputName" 
-                     onChange={(event) => setFormData({ ...formData, username: event.target.value })}
+                        color: "#FFFFFF",
+                        fontSize: pxToRem(20),
+                        fontWeight: 700,
+                     }}>Username:</TextLabel>
+                  <Input
+                     id="username"
+                     placeholder="João"
+                     className="inputName"
+                     onChange={handleChangeUser}
                      value={formData.username}
                      style={{
                         outline: 0,
@@ -70,18 +96,18 @@ export function FormSingIn() {
                         borderRadius: pxToRem(8),
                         background: "#EFF4F9",
                         color: "#000"
-                  }}/>
+                     }} />
 
                   <TextLabel style={{
                      color: "#FFFFFF",
                      fontSize: pxToRem(20),
                      fontWeight: 700,
                   }}>Senha:</TextLabel>
-
-                  <Input 
-                     placeholder="**********" 
-                     className="inputName" 
-                     onChange={(event) => setFormData({ ...formData, password: event.target.value })}
+                  <Input
+                     id="password"
+                     placeholder="**********"
+                     className="inputName"
+                     onChange={handleChangeUser}
                      value={formData.password}
                      type="password"
                      style={{
@@ -91,16 +117,18 @@ export function FormSingIn() {
                         borderRadius: pxToRem(8),
                         background: "#EFF4F9",
                         color: "#000"
-                  }}/>
+                     }} />
                </InputLabelContainer>
 
+               {formError && <Text color="#FF0000" fontSize="1rem" textAlign="center">Preencha todos os campos</Text>}
                <Button
                   className="button-submit"
                   onClick={handleSubmit}
                   style={{
-                  color: '#FFF',
-                  margin: "0 auto"
-               }}>Login</Button>
+                     color: '#FFF',
+                     margin: "auto auto",
+                     marginTop: "1rem",
+                  }}>Login</Button>
             </FormContainer>
 
             <Text color="#FFFFFF" fontSize={pxToRem(25)} style={{ textAlign: "center" }}>Entre para a festa você tambem!!</Text>
