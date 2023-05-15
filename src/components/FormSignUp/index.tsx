c086bfb63eb252f28e9a0005dfa816211c17b219
Vinicius Silva Pereira <viniciussilvapereira24@gmail.com>
2023-05-15 19:55:51 -0300

    Limpeza tela de cadastro

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -3,10 +3,6 @@ import { useHistory } from "react-router-dom";
 
 import carinhaMicrofone from "../../assets/images/carinhaComMic.svg";
 import segundaImage from "../../assets/images/segundaImage.svg";
-import TikTok from "../../assets/icons/TikTok.svg";
-import Instagram from "../../assets/icons/Instagram.svg";
-import Facebook from "../../assets/icons/Facebook.svg";
-import YouTube from "../../assets/icons/YouTube.svg";
 
 import { api } from "../../services/api.service";
 import { pxToRem } from "../../utils/convertToRem.util";
@@ -15,63 +11,15 @@ import { Input } from "../Input";
 import { Text } from "../Text";
 import { TextLabel } from "../TextLabel";
 
-import { CategoryContainer, Container, FormContainer, ImageContainer, InputLabelContainer, MidiaContainer, SocialContainer, ToggleWrapper } from "./styles";
+import { Container, FormContainer, ImageContainer, ToggleWrapper } from "./styles";
 import { Button } from "../Button";
 import { IUser } from "../../interfaces/IUser";
-import { IAddressId } from "../../interfaces/IAddressId";
-import { SignIn } from "../../pages/SignIn";
+
 import { Form as FormDeFora, Column } from "../../pages/Evento/styles";
 import { DadosContainer } from "../../pages/Profile/styles";
 import { Files, ImportFiles, Line } from "../../pages/Post/styles";
 import FileList from "../FileList";
-import { set } from "react-hook-form";
 
-// Tipagem da interface dos dados Sociais
-interface SocialProps {
-   id: string;
-   name?: string;
-   url: string;
-}
-
-// Tipagem da interface do componente de ícone
-interface IconComponentProps {
-   social: SocialProps;
-}
-
-// Tipagem das categorias
-interface CategoryProps {
-   id: string;
-   name: string;
-   type: string;
-}
-
-// Tipagem da interface do componente de categoria
-interface CategoryComponentProps {
-   category: CategoryProps;
-   onSelectCategory: (id: string) => void;
-}
-
-// Cria o componente de categoria
-export function CategoryComponent({ category, onSelectCategory }: CategoryComponentProps) {
-
-   return (
-      <>
-         <div className="category-item">
-            <Input
-               id={category.id}
-               type={"checkbox"}
-               className="category"
-               value={category.id}
-               color="#FFFFFF"
-               onChange={(event) => onSelectCategory(event.target.value)}
-            />
-            <Text fontSize={pxToRem(16)} color={"#FFFFFF"} >
-               {category.name}
-            </Text>
-         </div>
-      </>
-   )
-}
 
 export function Form() {
 
